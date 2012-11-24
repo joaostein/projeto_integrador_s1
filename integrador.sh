4c20cdeb34d4ba279b3646056b355690d1a96c7b
Joao Stein <joaosteing@gmail.com>
2012-11-24 08:02:26 -0200

    Added gitignore, created the while loop to receive all the variables related to registration, student name and also student grade and at end it appends on a new file called database.txt on the current path

--- integrador.sh
+++ integrador.sh
@@ -3,5 +3,31 @@
 clear
 
 date=`date`
+export_path="database.txt"
+
 
 echo "Data: $date"
+
+registration=000
+name=""
+grade=0
+
+while [[ $registration -ne 999 ]]; do
+  echo "Cadastro de alunos (999 p/ sair)"
+
+  echo "Matrícula: "
+  read registration
+
+  if [[ $registration -ne 999 ]]; then
+    echo "Nome: "
+    read name
+
+    echo "Nota: "
+    read grade
+
+    echo "$registration;$name;$grade" >> $export_path
+  else
+    clear
+    echo "Você saiu com sucesso."
+  fi
+done
